3e87a5f1afe0e5f52b9b18cd3f02f973914057f0
Dean <darobinson93@hotmail.com>
2022-06-01 22:48:41 +0100

    upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let playerSelection = "";
+playerSelection.toLowerCase;
+let computerSelection = computerPlay();
+
 function computerPlay() {
     const computerChoices = ["rock", "paper", "scissors"];
     const selection = Math.floor(Math.random() * computerChoices.length);
@@ -8,27 +15,54 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "rock") {
         return "It's a draw!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You win! Rock beats scissors!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "You lose! Paper beats rock!";
     } else if (playerSelection === "paper" && computerSelection === "paper") {
         return "It's a draw!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! paper beats rock!"
+        playerScore++;
+        return "You win! paper beats rock!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "You lose! Scissors beats paper!";
-    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    } else if (playerSelection === "scissors", computerSelection === "scissors") {
         return "It's a draw!";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Paper beats scissors!";
+        playerScore++;
+        return "You win! Scissors beats paper!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "You lose! Rock beats scissors!";
     } else {return "error"}
     
 }
 
-const playerSelection = "rock";
-playerSelection.toLowerCase;
-const computerSelection = computerPlay();
-
 console.log(playRound(playerSelection, computerSelection));
+
+
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = window.prompt("Please make a selection...");
+        let computerSelection = computerPlay();
+
+        console.log("Round #" + i);
+        console.log(playerScore);
+        console.log(computerScore);
+    if (i == 5) {
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You win the game!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("You lose! The computer has won the game!");
+    } else {console.log("It's a tie! Nobody wins!")}
+    } else {
+        playRound(playerSelection, computerSelection);
+    }
+   }
+}
+
+game();
